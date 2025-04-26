73eecb38fc06d3383abe040f31d752804463027d
dnjswlekd <wj8014@gmail.com>
2025-04-26 19:57:30 +0900

    [fix] router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import MainLayout from '@/layouts/MainLayout.vue';
 // import AuthLayout from '@/layouts/AuthLayout.vue';
 
@@ -42,7 +42,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
